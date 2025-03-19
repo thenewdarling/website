18349d8065d2d9ea5bbfb8d7857c8b824262f139
thenewdarling <54138886+thenewdarling@users.noreply.github.com>
2025-03-19 01:16:42 +0000

    init

---
+++ js/sidebar.js
@@ -0,0 +1,9 @@
+document.getElementById("navigationbar").innerHTML = `
+<nav class="navbar">
+    <ul>
+      <li><a href="/index.html">About Me</a></li>
+      <li><a href="/witches.html">Glasgow Witches</a></li>
+      <li><a href="/contact.html">Contact</a></li>
+    </ul>
+  </nav>
+`;
