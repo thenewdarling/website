d4d0949ef9e43ac91ca16da4e59124f73273e56b
thenewdarling <54138886+thenewdarling@users.noreply.github.com>
2025-03-19 11:25:04 +0000

    Basic layout of main pages, generic landing page added for witches series but not linked to, TODO series blurb

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,9 @@
 document.getElementById("navigationbar").innerHTML = `
 <nav class="navbar">
     <ul>
-      <li><a href="/index.html">About Me</a></li>
-      <li><a href="/witches.html">Glasgow Witches</a></li>
-      <li><a href="/contact.html">Contact</a></li>
+      <li><a href="/index.html">about me</a></li>
+      <li><a href="/witches/one.html">GLASGOW WITCHES</a></li>
+      <li><a href="/contact.html">contact</a></li>
     </ul>
   </nav>
 `;
