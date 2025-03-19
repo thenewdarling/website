a14fc499908a0e88aa6ee5fefb49343102620eba
thenewdarling <54138886+thenewdarling@users.noreply.github.com>
2025-03-19 20:22:37 +0000

    Simplification of CSS, fixing homepage text alignment manually
    CSS:
    - Additional unneeded statements now removed after I later fixed it for the whole site.
    - Actual commenting where things start and end.
    - Just added border-radius to all images since in practice I was putting it on everything anyway

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,8 +2,8 @@ document.getElementById("navigationbar").innerHTML = `
 <nav class="navbar">
     <ul>
       <li><a href="/index.html">about me</a></li>
-      <li><a href="/witches/one.html">GLASGOW WITCHES</a></li>
       <li><a href="/contact.html">contact</a></li>
+      <li><a href="/witches/one.html">GLASGOW WITCHES</a></li>
     </ul>
   </nav>
 `;
