894fa40c95263a4ef2f98e8c1c73817a72bb2ef8
thenewdarling <54138886+thenewdarling@users.noreply.github.com>
2025-06-18 10:32:39 +0100

    Change "/contact.html" to "/contact" and About me link to redirect to root URL

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,8 @@
 document.getElementById("navigationbar").innerHTML = `
 <nav class="navbar">
     <ul>
-      <li><a href="/index.html">about me</a></li>
-      <li><a href="/contact.html">contact</a></li>
+      <li><a href="/">about me</a></li>
+      <li><a href="/contact">contact</a></li>
       <li><a href="/witches/one.html">GLASGOW WITCHES</a></li>
     </ul>
   </nav>
